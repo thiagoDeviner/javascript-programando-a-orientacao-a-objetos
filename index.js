887e42d15fa31bfcdda126f38a93826602870b74
Thiago Vieira <thiagovieira.dg@gmail.com>
2020-11-18 15:47:27 -0300

    Criação do primeiro método

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
+    
     rg;
 }
 
+class ContaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+}
+
+
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
-const cliente3 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 cliente1.rg = 11122233310;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 cliente2.rg = 88822233310;
 
-cliente3.nome = "Gustavo";
-cliente3.cpf = 66622233309;
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
-cliente3.rg = 66622233310;
+const contaCorrenteRicardo = new ContaCorrente;
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(200);
+
+console.log(contaCorrenteRicardo.saldo);
 
 console.log(cliente1);
 console.log(cliente2);
-console.log(cliente3);
