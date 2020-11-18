9d19d20d8b34c21eda8de223d6dea469b5d401d4
Thiago Vieira <thiagovieira.dg@gmail.com>
2020-11-18 19:06:35 -0300

    Deixando a classe privada

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 class Cliente {
     nome;
     cpf;
-    
     rg;
 }
 
 class ContaCorrente {
+    // #saldo = 0 // https://github.com/tc39/proposal-class-fields#private-
+    fields
     agencia;
-    saldo;
+    _saldo = 0;
+
 
     sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
         }
     }
 }
@@ -29,15 +37,14 @@ cliente2.cpf = 88822233309;
 cliente2.rg = 88822233310;
 
 const contaCorrenteRicardo = new ContaCorrente;
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(200);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(1000);
 
-console.log(contaCorrenteRicardo.saldo);
 
-console.log(cliente1);
-console.log(cliente2);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo);
